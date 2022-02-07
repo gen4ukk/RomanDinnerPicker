a7e7b339eb1ca6792078ba032282fcbc9a0feebb
gen4ukk <gen4ukk@gmail.com>
2022-02-07 16:04:13 +0200

    add proj

---
+++ DinnerPicker/DinnerPicker/Models/DinnerPickerModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DinnerPicker.Models
+{
+    public class DinnerPickerModel
+    {
+        public int Room { get; set; }
+        public string Name { get; set; }
+        public string Dinner { get; set; }
+        public string Breakfast { get; set; }
+    }
+}
